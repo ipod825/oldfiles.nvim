0d6f167316e096eae73f3417e08ca6320e5fef6f
Shih-Ming Wang <swang150@ucsc.edu>
2022-05-01 22:13:38 -0700

    Refine.

--- lua/oldfiles.lua
+++ lua/oldfiles.lua
@@ -1,19 +1,16 @@
 local M = {}
-local path = require("libp.utils.path")
+local path = require("libp.path")
 
-local oldfiles_path = path.path_join(vim.fn.stdpath("data"), "oldfiles")
-local oldfiles
+local oldfiles_path = path.join(vim.fn.stdpath("data"), "oldfiles")
+local oldfiles = require("libp.datatype.Lru")(1000)
 
-local function initialize_oldfiles()
-	oldfiles = require("libp.datatype.Lru")(1000)
-	if vim.fn.filereadable(oldfiles_path) then
+function M.setup()
+	if vim.fn.filereadable(oldfiles_path) ~= 0 then
 		for _, f in ipairs(vim.fn.readfile(oldfiles_path)) do
 			oldfiles:add(f)
 		end
 	end
-end
 
-function M.setup()
 	local oldfiles_aug = vim.api.nvim_create_augroup("OLDFILES", {})
 	vim.api.nvim_create_autocmd("VimLeave", {
 		group = oldfiles_aug,
@@ -28,18 +25,12 @@ function M.setup()
 		group = oldfiles_aug,
 		pattern = "*",
 		callback = function(arg)
-			if not oldfiles then
-				initialize_oldfiles()
-			end
-			oldfiles:add(arg.file)
+			oldfiles:add(arg.match)
 		end,
 	})
 end
 
 function M.oldfiles()
-	if not oldfiles then
-		initialize_oldfiles()
-	end
 	return oldfiles:values()
 end
 
